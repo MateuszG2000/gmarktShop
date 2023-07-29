a8f8604e8df8f8dbedd5dbfd6c9b52a5a4a36389
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2023-07-29 11:33:39 +0200

    updated ErrorPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,5 @@
-import FooterSection from "./sections/FooterSection";
 import HomeSection from "./sections/HomeSection";
-import HeaderSection from "./sections/HeaderSection";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import NavbarComponent from "./components/HeaderComponents/NavbarComponent";
 import ProductComponent from "./components/ProductComponents/ProductComponent";
 import AuthSection from "./sections/AuthSection";
 import CartSecondStepSection from "./sections/CartSecondStepSection";
@@ -11,86 +8,6 @@ import CartThirdStepSection from "./sections/CartThirdStepSection";
 import CategorySection from "./sections/CategorySection";
 import RootLayout from "./pages/RootLayout";
 import ErrorPage from "./pages/ErrorPage";
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: (
-//       <>
-//         {" "}
-//         <HeaderSection />
-//         <NavbarComponent />
-//         <HomeSection />
-//         <FooterSection />
-//       </>
-//     ),
-//   },
-//   {
-//     path: "/x",
-//     element: (
-//       <>
-//         {" "}
-//         <HeaderSection />
-//         {/* <HomeSection /> */}
-//         <NavbarComponent />
-//         <ProductComponent />
-//         <FooterSection />
-//       </>
-//     ),
-//   },
-//   {
-//     path: "/cart",
-//     element: (
-//       <>
-//         {" "}
-//         <HeaderSection />
-//         <CartFirstStepSection />
-//         <FooterSection />
-//       </>
-//     ),
-//   },
-//   {
-//     path: "/log",
-//     element: (
-//       <>
-//         {" "}
-//         <HeaderSection />
-//         <AuthSection />
-//         <FooterSection />
-//       </>
-//     ),
-//   },
-//   {
-//     path: "/cart/data",
-//     element: (
-//       <>
-//         <HeaderSection />
-//         <CartSecondStepSection />
-//         <FooterSection />
-//       </>
-//     ),
-//   },
-//   {
-//     path: "/cart/summary",
-//     element: (
-//       <>
-//         <HeaderSection />
-//         <CartThirdStepSection />
-//         <FooterSection />
-//       </>
-//     ),
-//   },
-//   {
-//     path: "/monitory",
-//     element: (
-//       <>
-//         <HeaderSection />
-//         <NavbarComponent />
-//         <CategorySection />
-//         <FooterSection />
-//       </>
-//     ),
-//   },
-// ]);
 const router = createBrowserRouter([
   {
     path: "/",
--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 function ErrorPage() {
-  return <div>Nie tędy droga</div>;
+  return (
+    <div
+      style={{
+        fontSize: "2rem",
+        top: "50%",
+        left: "50%",
+        transform: "translateX(-50%) translateY(-50%)",
+        position: "absolute",
+        borderRadius: "0.2rem",
+        boxShadow: "3px 3px 13px -4px rgba(66, 68, 90, 1)",
+        padding: "3rem",
+      }}
+    >
+      Ta strona nie istnieje{" "}
+      <span
+        style={{
+          color: "red",
+          fontSize: "3rem",
+          marginLeft: "1rem",
+        }}
+      >
+        Error 404
+      </span>
+    </div>
+  );
 }
 
 export default ErrorPage;
