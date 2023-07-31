2bc6ba8cfc27a8ed1a0e27a2068eb62c7107d149
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2023-07-31 08:47:16 +0200

    changed place of .js files, added authoarized.ts

---
+++ backend/authorized.ts
@@ -0,0 +1,28 @@
+import { NextFunction } from "connect";
+import express from "express";
+
+const jwt = require("jsonwebtoken");
+
+const isAuth = function (
+  req: express.Request,
+  res: authResponse,
+  next: NextFunction
+) {
+  const authHeader = req.cookies.Authorization;
+  if (!authHeader) {
+    const error: Error = new Error("Not authorizated.");
+    error.statusCode = 401;
+    throw error;
+  }
+  const token = authHeader.trim();
+  try {
+    const decodedToken = jwt.verify(token, process.env.PRIVATE_KEY);
+    res.userID = decodedToken.userId;
+  } catch (err: any) {
+    err.statusCode = 500;
+    err.message = "Invalid Token";
+    throw err;
+  }
+  next();
+};
+module.exports = isAuth;
