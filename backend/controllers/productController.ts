13ce6c6a399562a1d9f47031e1cf4e56ffb06149
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2023-08-01 13:21:56 +0200

    created functions: delete, update, get one product, get all products

--- backend/controllers/productController.ts
+++ backend/controllers/productController.ts
@@ -12,7 +12,7 @@ export const createProduct = catchError(async function (
     data: newProduct,
   });
 });
-export const getProduct: ExpressFunction = catchError(async function (
+export const getProduct = catchError(async function (
   req: Request,
   res: Response,
   next: NextFunction
@@ -27,5 +27,51 @@ export const getProduct: ExpressFunction = catchError(async function (
   res.status(200).json({
     status: "success",
     data: product,
+  });
+});
+export const getProducts = catchError(async function (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const products = await Product.find();
+  res.status(200).json({
+    status: "success",
+    data: products,
+  });
+});
+export const updateProduct = catchError(async function (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!product) {
+    const error = new Error("There is no product with given ID");
+    error.statusCode = 404;
+    next(error);
+  }
+  res.status(200).json({
+    status: "success",
+    data: product,
+  });
+});
+export const deleteProduct = catchError(async function (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const product = await Product.findByIdAndUpdate(req.params.id);
+  if (!product) {
+    const error = new Error("There is no product with given ID");
+    error.statusCode = 404;
+    next(error);
+  }
+  res.status(200).json({
+    status: "success",
+    data: null,
   });
 });
--- backend/routes/productRoutes.ts
+++ backend/routes/productRoutes.ts
@@ -2,6 +2,12 @@ import Router from "express";
 const router = Router();
 import * as product from "../controllers/productController";
 
-router.post("/add", product.createProduct);
-router.get("/all:num", product.getProduct);
+router.get("/:id", product.getProduct);
+router
+  .route("/:id")
+  .get(product.getProduct)
+  .patch(product.updateProduct)
+  .delete(product.deleteProduct);
+router.get("/", product.getProducts);
+router.post("/", product.createProduct);
 export default router;
