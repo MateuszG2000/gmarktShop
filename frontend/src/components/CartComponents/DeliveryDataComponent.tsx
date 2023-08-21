2ead3b04e8bfd6961769858ad64cbad7981977cc
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2023-08-21 14:57:11 +0200

    addresses are read from object, added radio inputs

--- frontend/src/components/CartComponents/DeliveryDataComponent.tsx
+++ frontend/src/components/CartComponents/DeliveryDataComponent.tsx
@@ -1,17 +1,70 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import css from "./DeliveryDataComponent.module.scss";
 function DeliveryDataComponent() {
+  const AddressData = [
+    {
+      id: 1,
+      name: "Mateusz",
+      LastName: "G",
+      street: "Akacjowa 18",
+      code: "85-489",
+      city: "Łaziska",
+      tel: "731912912",
+      mail: "abc@wp.pl",
+    },
+    {
+      id: 2,
+      name: "Jacek",
+      LastName: "S",
+      street: "Kasztanowa 12",
+      code: "85-123",
+      city: "Opole",
+      tel: "123123123",
+      mail: "bca@onet.pl",
+    },
+  ];
+  const [Address, setAddress] = useState(0);
+  const clickHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setAddress(Number(event.target.id));
+  };
   return (
     <div className={css.deliveryData}>
       <p className={css.title}>Adres dostawy:</p>
-      <p>Mateusz G</p>
-      <p>ul. Akacjowa 18</p>
-      <p>85-489 Łaziska</p>
-      <br></br>
-      <p>tel. 721123987</p>
-      <p>e-mail: abc@wp.pl</p>
+      {AddressData.map((data, index) => {
+        return (
+          <React.Fragment key={data.id}>
+            <label htmlFor={`${index}`}>
+              <div className={css.label}>
+                <input
+                  type="radio"
+                  name="adress"
+                  id={`${index}`}
+                  checked={Address === index}
+                  onChange={clickHandler}
+                ></input>
+                <div className={css.address}>
+                  {" "}
+                  <p>
+                    {data.name} {data.LastName}
+                  </p>
+                  <p>ul. {data.street}</p>
+                  <p>
+                    {data.code} {data.city}
+                  </p>
+                </div>
+                <div className={css.contact}>
+                  {" "}
+                  <p>tel. {data.tel}</p>
+                  <p>e-mail: {data.mail}</p>
+                </div>
+              </div>
+            </label>
+          </React.Fragment>
+        );
+      })}
     </div>
   );
 }
 
 export default DeliveryDataComponent;
+<form className={css.deliveryMethodContainer}></form>;
