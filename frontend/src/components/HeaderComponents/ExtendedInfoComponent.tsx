82cda1241b7a52580293600941d17c8d3284c4a1
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2023-08-30 12:34:07 +0200

    moved useOutsideClick to separate file

--- frontend/src/components/HeaderComponents/ExtendedInfoComponent.tsx
+++ frontend/src/components/HeaderComponents/ExtendedInfoComponent.tsx
@@ -5,27 +5,16 @@ import { Link } from "react-router-dom";
 import { onLogOut } from "../../store/userAsync";
 import { useAppDispatch } from "../../store/appHooks";
 import { UIActions } from "../../store/UI";
+import useOutsideClick from "../../utils/useOutsideClick";
 function ExtendedInfoComponent() {
   const dispatch = useAppDispatch();
   const handleLogOut = () => {
     dispatch(UIActions.toggleAccountExtendedInfo());
     dispatch(onLogOut());
   };
-  function useOutsideAlerter(ref: any) {
-    useEffect(() => {
-      function handleClickOutside(event: any) {
-        if (ref.current && !ref.current.contains(event.target)) {
-          dispatch(UIActions.toggleAccountExtendedInfo());
-        }
-      }
-      document.addEventListener("mousedown", handleClickOutside);
-      return () => {
-        document.removeEventListener("mousedown", handleClickOutside);
-      };
-    }, [ref]);
-  }
+
   const wrapperRef = useRef(null);
-  useOutsideAlerter(wrapperRef);
+  useOutsideClick(wrapperRef);
   return (
     <div ref={wrapperRef} className={`${css.infoContainer} `}>
       <p className={css.title}>Witaj Mateusz</p>
