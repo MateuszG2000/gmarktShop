4a12eced85cead9194fa0acf960f0aaa5f1f0060
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2023-08-30 23:05:57 +0200

    adding validation - in progress

--- frontend/src/components/AuthComponents/ErrorComponent.tsx
+++ frontend/src/components/AuthComponents/ErrorComponent.tsx
@@ -3,6 +3,6 @@ import css from "./ErrorComponent.module.scss";
 const ErrorComponent: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  return <p className={css.title}>{children}</p>;
+  return <div className={css.title}>{children}</div>;
 };
 export default ErrorComponent;
--- frontend/src/components/AuthComponents/SignUp.tsx
+++ frontend/src/components/AuthComponents/SignUp.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import css from "./SignUp.module.scss";
 import ButtonComponent from "../CommonComponents/ButtonComponent";
 import * as validator from "../../utils/validators";
 import useInput from "../../utils/use-input";
 import Input from "./Input";
+import ErrorComponent from "./ErrorComponent";
 function SignUp() {
+  const radioHandler = function (e: React.ChangeEvent<HTMLInputElement>) {
+    if (e.target.checked) {
+      setChecked(true);
+    } else setChecked(false);
+  };
+  const [checked, setChecked] = useState(false);
   const {
     value: enteredEmail,
     isValid: enteredEmailIsValid,
@@ -20,7 +27,16 @@ function SignUp() {
     valueChangeHandler: passwordChangeHandler,
     inputBlurHandler: passwordBlurHandler,
     reset: resetPasswordInput,
-  } = useInput((value: string) => validator.required(value));
+  } = useInput((value: string) => validator.length(value, { min: 8, max: 20 }));
+  const {
+    value: enteredPasswordConfirm,
+    isValid: enteredPasswordConfirmIsValid,
+    hasError: passwordConfirmInputHasError,
+    valueChangeHandler: passwordConfirmChangeHandler,
+    inputBlurHandler: passwordConfirmBlurHandler,
+    reset: resetPasswordConfirmInput,
+  } = useInput((value: string) => validator.isSame(value, enteredPassword));
+
   return (
     <div className={css.loginPanel}>
       <form className={css.loginForm}>
@@ -50,22 +66,46 @@ function SignUp() {
         ></Input>
         <Input
           id="password-input-confirm"
-          type="password-confirm"
+          type="password"
           className="input"
           name="password-confirm"
           title="Powtórz hasło:"
-          value={enteredPassword}
-          valid={enteredPasswordIsValid}
-          touched={passwordInputHasError}
-          onBlur={passwordBlurHandler}
-          onChange={passwordChangeHandler}
+          value={enteredPasswordConfirm}
+          valid={enteredPasswordConfirmIsValid}
+          touched={passwordConfirmInputHasError}
+          onBlur={passwordConfirmBlurHandler}
+          onChange={passwordConfirmChangeHandler}
         ></Input>
         <p>
-          <input type="checkbox" id="regulamin" name="regulamin"></input>
+          <input
+            onChange={radioHandler}
+            type="checkbox"
+            id="regulamin"
+            name="regulamin"
+          ></input>
           <label htmlFor="regulamin">Akceptuję Regulamin</label>
         </p>
+        <ErrorComponent>
+          {emailInputHasError && !enteredEmailIsValid && (
+            <p>Nieprawidłowy e-mail</p>
+          )}
+          {passwordInputHasError && !enteredPasswordIsValid && (
+            <p>Hasło musi zawierać od 8 do 20 znaków</p>
+          )}
+          {passwordConfirmInputHasError && !enteredPasswordConfirmIsValid && (
+            <p>Hasła się nie zgadzają</p>
+          )}
+          {!checked && <p>Akceptacja regulaminu jest wymagana</p>}
+        </ErrorComponent>
         <ButtonComponent
-          disabled={!(enteredEmailIsValid && enteredPasswordIsValid)}
+          disabled={
+            !(
+              enteredEmailIsValid &&
+              enteredPasswordIsValid &&
+              enteredPasswordConfirmIsValid &&
+              checked
+            )
+          }
         >
           Zarejestruj się
         </ButtonComponent>
