269b85296e06751d7947800185de59c8e1d4d176
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2023-08-30 16:39:11 +0200

    added: search

--- frontend/src/components/HeaderComponents/SearchComponent.tsx
+++ frontend/src/components/HeaderComponents/SearchComponent.tsx
@@ -1,35 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import css from "./SearchComponent.module.scss";
-// import monitor from "../../Photos/i-xiaomi-mi-curved-gaming-34-bhr4269gl.jpg.webp";
+const debounce = require("lodash.debounce");
+
 function SearchComponent() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [isSearching, setIsSearching] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const searchHandler = debounce(
+    async (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (e.target.value == "") {
+        setIsSearching(false);
+        setIsLoading(false);
+      } else {
+        setIsLoading(true);
+        setIsSearching(true);
+      }
+
+      const response = await (
+        await fetch(
+          `http://localhost:9000/api/product/?name=${e.target.value}&fields=name,_id,image`
+        )
+      ).json();
+      setProducts(response.data);
+      setIsLoading(false);
+    },
+    500
+  );
   return (
     <div className={css.search}>
       <form action="#">
         <input
-          className={`${css.default} ${css.first}`}
-          // className={css.first}
+          className={`${isSearching ? css.first : css.default}`}
           type="text"
           placeholder="Wyszukaj..."
+          onChange={searchHandler}
+          onBlur={() => {
+            setIsSearching(false);
+          }}
         />
       </form>
-      {/* <div className={css.resultContainer}>
-        <div className={`${css.result} ${css.inside}`}>
-          <img className={css.img} src={monitor} alt="abc"></img>
-          <span className={css.title}>Xiaomi Mi Curved Gaming s</span>
+      {isSearching && (
+        <div className={css.resultContainer}>
+          {products.map((item, index) => (
+            <div
+              key={item._id}
+              className={`${css.result} ${css.inside} ${
+                index + 1 === products.length ? css.last : ""
+              }`}
+            >
+              <img
+                className={css.image}
+                src={`http://localhost:9000/api/images/${item.image}`}
+                alt={String(item.name)}
+              ></img>
+              <span className={css.title}>{item.name}</span>
+            </div>
+          ))}
         </div>
-        <div className={`${css.result} ${css.inside}`}>
-          <img className={css.img} src={monitor} alt="abc"></img>
-          <span className={css.title}>Xiaomi Mi Curved Gaming s</span>
+      )}
+      {isLoading && (
+        <div className={css.resultContainer}>
+          <div className={`${css.result} ${css.inside} ${css.last}`}>
+            <span className={css.title}>Szukam...</span>
+          </div>
         </div>
-        <div className={`${css.result} ${css.inside}`}>
-          <img className={css.img} src={monitor} alt="abc"></img>
-          <span className={css.title}>Xiaomi Mi Curved Gaming s</span>
+      )}
+      {products.length === 0 && isSearching && (
+        <div className={css.resultContainer}>
+          <div className={`${css.result} ${css.inside} ${css.last}`}>
+            <span className={css.title}>Brak produktu</span>
+          </div>
         </div>
-        <div className={`${css.result} ${css.last}`}>
-          <img className={css.img} src={monitor} alt="abc"></img>
-          <span className={css.title}>Xiaomi Mi Curved Gaming s</span>
-        </div>
-      </div> */}
+      )}
     </div>
   );
 }
