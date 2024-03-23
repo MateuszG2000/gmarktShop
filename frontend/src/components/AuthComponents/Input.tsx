af0f9469441f75c26fa28d90c3f2c3be544e1648
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2024-03-23 14:52:22 +0100

    refactored inputs

--- frontend/src/components/AuthComponents/Input.tsx
+++ frontend/src/components/AuthComponents/Input.tsx
@@ -2,7 +2,8 @@ import React, { ChangeEventHandler, FocusEventHandler } from "react";
 import css from "./Input.module.scss";
 
 function Input(props: InputProps) {
-  return (
+  let markup;
+  const normalInput = (
     <>
       {props.title !== undefined && <label htmlFor={props.name}>{props.title}</label>}
       <input
@@ -20,22 +21,83 @@ function Input(props: InputProps) {
       ></input>
     </>
   );
+  const selectCategory = (
+    <select
+      id={props.id}
+      className={`${css[props.className]} ${!props.valid ? css.invalid : ""} ${props.touched ? css.touched : ""}`}
+      name={props.name}
+      value={props.value}
+      onChange={props.onChange}
+    >
+      <option value="">Kategoria</option>
+      <option value="pcs">Komputery stacjonarne</option>
+      <option value="monitors">Monitory</option>
+      <option value="laptops">Laptopy</option>
+      <option value="headphones">Słuchawki</option>
+      <option value="phones">Smartfony</option>
+      <option value="accessories">Akcesoria</option>
+    </select>
+  );
+  const selectWeight = (
+    <select
+      id={props.id}
+      className={`${css[props.className]} ${!props.valid ? css.invalid : ""} ${props.touched ? css.touched : ""}`}
+      name={props.name}
+      value={props.value}
+      onChange={props.onChange}
+    >
+      <option value="">Waga</option>
+      <option value={1}>1</option>
+      <option value={2}>2</option>
+      <option value={3}>3</option>
+      <option value={4}>4</option>
+      <option value={5}>5</option>
+    </select>
+  );
+  const selectQuantity = (
+    <select
+      id={props.id}
+      className={`${css[props.className]} ${!props.valid ? css.invalid : ""} ${props.touched ? css.touched : ""}`}
+      name={props.name}
+      value={props.value}
+      onChange={props.onChange}
+    >
+      <option value="">Ilość</option>
+      <option value="more">więcej</option>
+      <option value="less">mniej</option>
+    </select>
+  );
+
+  const switchBtn = (
+    <label className={css.switch}>
+      <input name={props.name} value={props.value} onChange={props.onChange} type="checkbox" />
+      <span className={`${css.slider} ${css.round}`}></span>
+    </label>
+  );
+  console.log();
+  if (props.customType === "selectCategory") markup = selectCategory;
+  else if (props.customType === "selectWeight") markup = selectWeight;
+  else if (props.customType === "selectQuantity") markup = selectQuantity;
+  else if (props.customType === "switchBtn") markup = switchBtn;
+  else markup = normalInput;
+  return markup;
 }
 
 export default Input;
 type InputProps = {
+  customType?: string;
   name: string;
   title?: string;
   className: string;
   valid?: boolean;
   touched?: boolean;
   id: string;
-  type: string;
+  type?: string;
   value?: string;
   placeholder?: string;
   step?: number;
-  onBlur?: FocusEventHandler<HTMLInputElement>;
-  onChange?: ChangeEventHandler<HTMLInputElement>;
+  onBlur?: FocusEventHandler<HTMLInputElement | HTMLSelectElement>;
+  onChange?: ChangeEventHandler<HTMLInputElement | HTMLSelectElement>;
   min?: number;
   max?: number;
 };
--- frontend/src/components/UserPanelComponents/ProductsMatching.tsx
+++ frontend/src/components/UserPanelComponents/ProductsMatching.tsx
@@ -2,10 +2,18 @@ import React, { BaseSyntheticEvent, useState } from "react";
 import { useAppDispatch } from "../../store/appHooks";
 import Input from "../AuthComponents/Input";
 import css from "./ProductsMatching.module.scss";
+
 function ProductsMatching() {
   const [formData, setFormData] = useState<any>({});
   const dispatch = useAppDispatch();
-  const onSubmitHandler = async function (e: BaseSyntheticEvent) {};
+
+  const handleChange = (e: BaseSyntheticEvent) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmitHandler = async function (e: BaseSyntheticEvent) {
+    // Obsługa zdarzenia onSubmit
+  };
 
   return (
     <div className={css.panel}>
@@ -13,51 +21,38 @@ function ProductsMatching() {
       <form onSubmit={onSubmitHandler} className={css.form}>
         <div className={css.title}>Dopasowanie według płci:</div>
         <div className={css.option}>
-          <label className={css.switch}>
-            <input type="checkbox" />
-            <span className={`${css.slider} ${css.round}`}></span>
-          </label>
-          <span>Mężczyzna: </span>
-          <select
-            id="quantity-input"
-            name="quantity-input"
+          <Input
+            customType="switchBtn"
+            name="gender"
+            value={formData.gender}
+            onChange={handleChange}
             className="input"
-            value={formData.quantity}
-            onChange={(e: BaseSyntheticEvent) => setFormData({ ...formData, quantity: e.target.value })}
-          >
-            <option value="">Ilość</option>
-            <option value="more">więcej</option>
-            <option value="less">mniej</option>
-          </select>
-          <select
-            id="category-input"
-            name="category-input"
-            className="input"
+            id="gender-input"
+          />
+          <Input
+            customType="selectCategory"
+            name="category"
             value={formData.category}
-            onChange={(e: BaseSyntheticEvent) => setFormData({ ...formData, category: e.target.value })}
-          >
-            <option value="">Kategoria</option>
-            <option value="pcs">Komputery stacjonarne</option>
-            <option value="monitors">Monitory</option>
-            <option value="laptops">Laptopy</option>
-            <option value="headphones">Słuchawki</option>
-            <option value="phones">Smartfony</option>
-            <option value="accessories">Akcesoria</option>
-          </select>
-          <select
-            id="weight-input"
-            name="weight-input"
+            onChange={handleChange}
             className="input"
+            id="category-input"
+          />
+          <Input
+            customType="selectWeight"
+            name="weight"
             value={formData.weight}
-            onChange={(e: BaseSyntheticEvent) => setFormData({ ...formData, weight: e.target.value })}
-          >
-            <option value="">Waga</option>
-            <option value={1}>1</option>
-            <option value={2}>2</option>
-            <option value={3}>3</option>
-            <option value={4}>4</option>
-            <option value={5}>5</option>
-          </select>
+            onChange={handleChange}
+            className="input"
+            id="weight-input"
+          />
+          <Input
+            customType="selectQuantity"
+            name="quantity"
+            value={formData.quantity}
+            onChange={handleChange}
+            className="input"
+            id="quantity-input"
+          />
         </div>
       </form>
     </div>
