e5c8536718c6469119899fb8f38aa15904a0746e
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2024-03-28 18:45:26 +0100

    fix route bug

--- backend/routes/productRoutes.ts
+++ backend/routes/productRoutes.ts
@@ -3,8 +3,8 @@ const router = Router();
 import * as product from '../controllers/productController';
 import * as user from '../controllers/userController';
 
-router.route('/:id').get(product.getProduct).patch(product.updateProduct);
 router.get('/match', product.getProductsMatch);
+router.route('/:id').get(product.getProduct).patch(product.updateProduct);
 router.get('/', product.getProducts);
 
 router.delete('/:id', user.isAuth('admin2'), product.deleteProduct);
